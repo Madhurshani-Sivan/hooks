4129f7db18aaa0f63514f374cd1bae6bf21e03a0
Madhurshani-Sivan <madhurshanisivan@gmail.com>
2023-09-08 09:39:49 +0530

    load data using useEffect

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import IngredientForm from "./IngredientForm";
 import Search from "./Search";
@@ -6,6 +6,25 @@ import IngredientList from "./IngredientList";
 
 const Ingredients = () => {
   const [userIngredients, setUserIngrediants] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://react-hooks-update-ed11d-default-rtdb.firebaseio.com/ingredients.json"
+    )
+      .then((response) => response.json())
+      .then((responseData) => {
+        const loadedIngredients = [];
+
+        for (const key in responseData) {
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount,
+          });
+        }
+        setUserIngrediants(loadedIngredients);
+      });
+  }, []);
 
   const addIngredientHandler = (ingredient) => {
     fetch(
