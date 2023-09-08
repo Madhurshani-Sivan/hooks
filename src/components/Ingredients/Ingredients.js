00857213c8e9e0b4ff2b63909b4238aced9ffd2c
Madhurshani-Sivan <madhurshanisivan@gmail.com>
2023-09-08 10:18:37 +0530

    stop infinite looping using callback

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
 import IngredientForm from "./IngredientForm";
 import Search from "./Search";
@@ -8,31 +8,12 @@ const Ingredients = () => {
   const [userIngredients, setUserIngrediants] = useState([]);
 
   useEffect(() => {
-    fetch(
-      "https://react-hooks-update-ed11d-default-rtdb.firebaseio.com/ingredients.json"
-    )
-      .then((response) => response.json())
-      .then((responseData) => {
-        const loadedIngredients = [];
-
-        for (const key in responseData) {
-          loadedIngredients.push({
-            id: key,
-            title: responseData[key].title,
-            amount: responseData[key].amount,
-          });
-        }
-        setUserIngrediants(loadedIngredients);
-      });
-  }, []);
-
-  useEffect(() => {
     console.log("Rendering ingrdients", userIngredients);
   }, [userIngredients]);
 
-  const filteredIngredientsHandler = (filteredIngredients) => {
+  const filteredIngredientsHandler = useCallback((filteredIngredients) => {
     setUserIngrediants(filteredIngredients);
-  };
+  }, []);
 
   const addIngredientHandler = (ingredient) => {
     fetch(
--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -29,7 +29,7 @@ const Search = React.memo((props) => {
             amount: responseData[key].amount,
           });
         }
-        //onLoadIngredients(loadedIngredients);
+        onLoadIngredients(loadedIngredients);
       });
   }, [enteredFilter, onLoadIngredients]);
 
